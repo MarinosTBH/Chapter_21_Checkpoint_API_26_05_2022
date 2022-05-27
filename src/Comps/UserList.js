aaeb8bd1b9857c6754fdf15a85ca4c68b4bb785a
MarinosTBH <marinostbh@gmail.com>
2022-05-27 21:49:32 +0100

    first api app test

---
+++ src/Comps/UserList.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React,{useState, useEffect} from 'react'
+import "../App.css"
+
+const UserList = () => {
+    const avatarMale = "https://www.bootdey.com/img/Content/avatar/avatar7.png";
+    const avatarFemale = "https://www.bootdey.com/img/Content/avatar/avatar3.png";
+    
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+          setUsers(res.data)
+        } catch (error) {
+          console.log(error)
+        }
+  
+      }
+      fetchData()
+    }, [])
+  return (
+    <div className="task-box">
+        <ul >{users.map(user=> 
+            <div className="todo-row" key={user.id}>
+                <h3>{user.name}</h3>
+                <p>{user.email}</p>
+                <div className="icon-box"><img className="avatar" src={user.id%2===0? avatarMale:avatarFemale}/></div>
+            </div>
+        
+        )}
+        </ul>
+    </div>
+  )
+}
+
+export default UserList
